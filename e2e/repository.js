3323c7f76d0e3543fbc3d500ce7998c9d0891d5c
Dan Thomas <Dan.Thomas@ao.com>
2015-02-16 17:30:41 +0000

    asdfasdf

---
+++ e2e/repository.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    "use strict";
+
+    var repository = function () {
+
+        this.MongoClient = require('mongodb').MongoClient;
+
+        this.addAccount = function (accountname, password) {
+
+            var account = { accountname: accountname, password: password };
+
+            this.MongoClient.connect('mongodb://127.0.0.1:27017/e2e', function (err, db) {
+                if (err) throw err;
+
+                var collection = db.collection('account');
+
+                collection.insert(account, function (err, docs) {
+
+                });
+
+                db.close();
+            });
+        };
+
+        this.getAccount = function (accountName) {
+            this.MongoClient.connect('mongodb://127.0.0.1:27017/e2e', function (err, db) {
+                if (err) throw err;
+
+                var collection = db.collection('account');
+
+                collection.find({ accountName: accountName }).toArray(function (err, results) {
+                    if (err) throw err;
+
+
+                    if (results.length === 0) {
+                        return null;
+                    } else {
+                        return results[0];
+                    }
+                });
+                db.close();
+            });
+        }
+    };
+
+    module.exports = repository;
+}());
--- e2e/server.js
+++ e2e/server.js
@@ -1,7 +1,64 @@
 ﻿var express = require('express');
 var serveStatic = require('serve-static');
+var bodyParser = require('body-parser');
+var Repository = require('./repository.js');
+
+
 var app = express()
-.use(serveStatic(__dirname + '/app'))
-.use(serveStatic(__dirname + '/e2eSpecs')) //only required for testing
-.use(serveStatic(__dirname + '/node_modules'))
-.listen(3000);
+    .use(bodyParser.json())
+    .use(serveStatic(__dirname + '/app'))
+    .use(serveStatic(__dirname + '/e2eSpecs')) //only required for testing
+    .use(serveStatic(__dirname + '/node_modules'))
+
+    .use(function (req, res) {
+
+
+        var repository = new Repository();
+
+        if (req.path === '/login') {
+
+            var account = repository.getAccount(req.body.accountName);
+
+            res.end('{success: ' + (account == null ? 'false' : 'true') + '}');
+            return;
+        }
+
+        if (req.body.foo) {
+
+
+            repository.addAccount('asdfdf', 'asdfasdf');
+
+            //res.end(testing());
+        }
+        else {
+            res.end('Body does not have foo!');
+        }
+    })
+    .use(function (err, req, res, next) {
+        res.end('Invalid body!');
+    })
+    .listen(3000);
+/*
+
+var testing = function () {
+
+    var account = { username: 'thomasd', password: 'password' };
+    //var findKey = { name: 'John' };
+    MongoClient.connect('mongodb://127.0.0.1:27017/e2e', function(err, db) {
+        if (err) throw err;
+        console.log('Successfully connected');
+        var collection = db.collection('account');
+        collection.insert(account, function(err, docs) {
+            console.log('Inserted', docs[0]);
+            console.log('ID:', account._id);
+//            collection.find(findKey).toArray(function(err, results) {
+//                console.log('Found results:', results);
+//                collection.remove(findKey, function(err, results) {
+//                    console.log('Deleted person');
+//                    db.close();
+//                });
+//            });
+        });
+    });
+};
+*/
